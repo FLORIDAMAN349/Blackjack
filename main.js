aa670e198f7fc8182a08e2534b52c037fa9925ca
Unpei2 <r.ho5@share.epsb.ca>
2022-10-31 14:49:29 -0600

    basic

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hi")
+
+
+let dealerdisplay = document.getElementById("dealer")
+let playerdisplay = document.getElementById("player")
+let dealerhand = []
+let playerhand = []
+// pick cards from 1-13
+for (x=0;x < 2; x++){
+    let pick = Math.floor(Math.random() * (14 - 1) + 1)
+    dealerhand.push(pick)
+    dealerdisplay.innerHTML = `?, ${dealerhand[1]}`
+}
+for (x=0;x < 2; x++){
+    let pick = Math.floor(Math.random() * (14 - 1) + 1)
+    playerhand.push(pick)
+    playerdisplay.innerHTML = playerhand
+}
+
+console.log(dealerhand)
