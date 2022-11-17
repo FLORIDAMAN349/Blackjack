b0d9dd14716a7cfecf7adcafeda96137d4519ade
Unpei2 <r.ho5@share.epsb.ca>
2022-11-17 14:49:51 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,11 +6,16 @@ let hit = document.getElementById("hit")
 let stand = document.getElementById("stand")
 hit.addEventListener("click", clickhit)
 stand.addEventListener("click",clickstand)
-let cards = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+let j = 10
+let q = 10
+let k = 10
+let a = 1
+let cards = [a,2,3,4,5,6,7,8,9,10,j,q,k]
 let dealerhand = []
 let playerhand = []
 let stophit = false
 let stopstand = false
+let playerhandnum = 0
 // pick cards from 1-13
 for (x=0;x < 2; x++){
     
@@ -25,26 +30,32 @@ for (x=0;x < 2; x++){
 
 function clickhit(){
     if (stopstand){
+        alert("Can't Press this again")
         return
     }
     if (stophit){
         alert("Bust! Dealer Wins")
         return
     }
-    let playerhandnum = 0
+    checknum()
     playerhand.push(random())
-    for (x = 0;x < playerhand.length;x++){
-        playerhandnum += playerhand[x]
-        console.log(playerhandnum)
-        if (playerhandnum > 21){
+    if (checknum() > 21){
             alert("Bust! Dealer Wins")
             playerdisplay.innerHTML = playerhand 
             stophit = true
+            stopstand = true
             return
-        }  
+        
     }
+    
     playerdisplay.innerHTML = playerhand 
 
+}
+function checknum(){
+    for (x = 0;x < playerhand.length;x++){
+        playerhandnum += playerhand[x]
+    }
+    return playerhandnum
 }
 function clickstand(){
     if (stopstand){
@@ -54,17 +65,15 @@ function clickstand(){
         alert("Bust! Dealer Wins")
         return
     }
-    let dealerhandnum = 0
+    dealerhandnum = 0
     for (x = 0;x<dealerhand.length;x++){
         dealerhandnum += dealerhand[x]
-        console.log(dealerhandnum)
-        
+        add(dealerhandnum)
     }
-    if (dealerhandnum <= 15){
-        dealerhand.push(random())
-    }
+    console.log(dealerhandnum)
+    console.log(playerhandnum)
       
-    else if (dealerhandnum > 21){
+    if (dealerhandnum > 21){
         alert("Bust! Player wins")
         dealerdisplay.innerHTML = dealerhand
         return
@@ -74,18 +83,27 @@ function clickstand(){
     }  
     if (dealerhandnum > playerhandnum){
         alert("Dealer Wins!")
+    }   else if (playerhandnum === dealerhandnum){
+        alert("draw")
     }   else {
         alert("Player Wins!")
     }
     dealerdisplay.innerHTML = dealerhand
     stopstand = true
+    stophit = true
+}
+function add(number){
+    if (number <= 15){
+        dealerhand.push(random())
+    }
 }
 function random(){
     let random = Math.floor(Math.random() * 13)
     return cards[random]
 }
-
+function winner (w,e){
+    
+}
 console.log(dealerhand)
 console.log(playerhand)
 
-   
